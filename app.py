1f1594ed906bfd1944d26441424e412a3915763a
atulbhatt-system32 <atulbhatt98@gmail.com>
2020-04-09 16:50:07 +0530

    10% completed

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
+
+@app.route("/index", methods=['GET', 'POST'])
+def index():
+    return render_template("Home.html")
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        print("hello")
+        
+    return render_template("Home.html")
